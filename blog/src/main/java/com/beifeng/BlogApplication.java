14b96733d316c856bd4b7b18a1f621921d375531
BeiFeng <1611606790@qq.com>
2020-10-28 16:41:32 +0800

    日志处理

--- blog/src/main/java/com/beifeng/BlogApplication.java
+++ blog/src/main/java/com/beifeng/BlogApplication.java
@@ -3,6 +3,11 @@ package com.beifeng;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author BeiFeng
+ * @version 1.0
+ * @date 2020/10/26 16:09
+ */
 @SpringBootApplication
 public class BlogApplication {
 
--- blog/src/main/java/com/beifeng/web/controller/IndexController.java
+++ blog/src/main/java/com/beifeng/web/controller/IndexController.java
@@ -8,19 +8,21 @@ package com.beifeng.web.controller;
 import com.beifeng.execption.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
+    @GetMapping("/{id}/{name}")
     // @ResponseBody
-    public String index(){
+    public String index(@PathVariable Integer id,@PathVariable String name){
         // int i = 9/0;
-        String blog = null;
+        /*String blog = null;
         if (blog == null){
             throw new NotFoundException("博客不存在");
-        }
+        }*/
+        System.out.println("-----------index-------------");
         return "index";
     }
 }
